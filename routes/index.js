2415c9739a42ebccbebda94c5617aef77cf78df5
chuckiechou <zzp208@qq.com>
2019-07-14 19:06:17 +0800

    add nodemon,router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', function (req, res, next) {
+  res.send(req.url);
 });
 
 module.exports = router;
